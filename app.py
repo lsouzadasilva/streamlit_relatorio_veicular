e685bd61a5573fb6a639117195852c298f84525d
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-08 23:59:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,11 @@ with filtro:
     df_filtro = filtro_tela(df)
 
 st.sidebar.divider()
-st.sidebar.markdown("Desenvolvido por [Leandro Souza](https://br.linkedin.com/in/leandro-souza-313136190)")
+st.sidebar.markdown("""
+    **Desenvolvido por Leandro Souza**  
+    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
+    [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva)
+""")
 
 
 if paginas == 'Veículos':
