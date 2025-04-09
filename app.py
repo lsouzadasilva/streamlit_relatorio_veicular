e4917940cf53d390e4fe814f24cdbfb74629dda7
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-09 00:00:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.sidebar.divider()
 st.sidebar.markdown("""
     **Desenvolvido por Leandro Souza**  
     [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
-    [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva)
+    [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/streamlit_relatorio_veicular)
 """)
 
 
