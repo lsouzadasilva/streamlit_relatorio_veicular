9976c4a2f5990142a58e0dc3ed6fe4aad7789785
Leandro Souza <lsouzadasilva4@gmail.com>
2025-07-18 13:59:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with filtro:
 st.sidebar.divider()
 st.sidebar.markdown("""
     **Desenvolvido por Leandro Souza**  
-    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
+    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in//leandro-souza-dados/)
     [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/streamlit_relatorio_veicular)
 """)
 
